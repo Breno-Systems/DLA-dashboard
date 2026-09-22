c59441a3a9597e21e5a52bc66e654aedc2dfd3a8
Breno Gabriel <breno.systems@outlook.com>
2026-09-22 11:40:44 -0300

    Remove unused import from app.py
    Remove unused import for ServiceAccountCredentials.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import plotly.express as px
 
 URL = st.secrets(URL_PLANILHA)
