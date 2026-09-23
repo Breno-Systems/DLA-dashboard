66bb70bcfcde2530664a769365f5dcb3c8c45b7c
Breno Gabriel <breno.systems@outlook.com>
2026-09-23 07:28:58 -0300

    Configure Streamlit page and improve data handling
    Added Streamlit page configuration and data processing enhancements.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import pandas as pd
-import gspread
 import plotly.express as px
+
+st.set_page_config(
+    page_title="Dashboard de Engajamento",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={"About":"Dashboard de Engajamento"}
+)
+
 
 URL = st.secrets["URL_PLANILHA"]
 
@@ -15,12 +22,15 @@ df = carregar_dados()
 # DF Config
 df.columns = ["Data", "Turma", "Aulas", "Engajamento"]
 
-st.set_page_config(
-    page_title="Dashboard de Engajamento",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={"About":"Dashboard de Engajamento"}
-)
+  # Data -> Datetime
+df["Data"] = pd.to_datetime(df["Data"], dayfirst=True)
+
+  # Mapeamento de Score
+df["score"] = df["Engajamento"].map({"Alto": 3, "Médio": 2, "Baixo": 1})
+
+if df.empty:
+  st.warning("⚠️ Nenhuma resposta ainda. Preencha o formulário para ver os dados.")
+  st.stop()
 
 st.title("Dashboard de Engajamento")
 st.caption("Escola Estadual - Ensino Fundamental II e Ensino Médio")
