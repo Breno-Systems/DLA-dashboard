d2b1735b7f08bbf3e0fff07641aa75401cc9319f
Breno Gabriel <breno.systems@outlook.com>
2026-09-23 09:05:46 -0300

    Set y-axis range for engagement evolution chart

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ fig = px.line(
     markers=True,  # pontos visíveis
     title="Evolução do Engajamento"
 )
-fig.update_yaxes()
+fig.update_yaxes(range=[0, 3.2])
 
 st.subheader("Evolução do engajamento")
 st.plotly_chart(fig, width="stretch")
