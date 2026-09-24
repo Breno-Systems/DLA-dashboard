06e5227421fccbaad122e37439c6a24d74e9fafc
Breno Gabriel <breno.systems@outlook.com>
2026-09-24 09:05:37 -0300

    Update dataframe styling in app.py
    Refactor dataframe styling for better readability.

--- app.py
+++ app.py
@@ -187,10 +187,10 @@ with st.expander ("📋 Ver dados brutos"):
     df_view = df_filtrado.sort_values("Data", ascending=False).copy()
     df_view["Data"] = df_view["Data"].dt.strftime("%d/%m/%Y")
     df_view = df_view.reset_index(drop=True)
+    styled = (df_view.style.map(colorir, subset=["Engajamento"]).hide(axis="index"),
     st.dataframe(
-        df_view.style.map(colorir, subset=["Engajamento"]),
-        height=300,
-        hide_index=True
+        styled,
+        height=300
     )
 
 
